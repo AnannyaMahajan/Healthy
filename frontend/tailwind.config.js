54ac97b2d3d0126ae25394c615fdb171b393fea2
Anannya Mahajan <anannyamahajan00@gmail.com>
2025-09-13 18:57:16 +0530

    Create tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2b6cb0",
+        accent: "#16a34a"
+      }
+    },
+  },
+  plugins: [],
+};
